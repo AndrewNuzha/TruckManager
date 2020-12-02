d92b4d7ed0f68753b9f368ff87554e659801a630
Andrei Nuzha <andrey.nuzha@gmail.com>
2020-12-03 02:17:00 +0300

    initial commit

---
+++ src/main/java/com/myprojects/truckmanager/truckManagerApp/TruckManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.truckmanager.truckManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckManagerAppApplication.class, args);
+	}
+
+}
